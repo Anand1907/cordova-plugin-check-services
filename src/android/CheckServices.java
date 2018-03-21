07bcce49df928a9298118500d01addcc69b66a43
Anand1907 <anandmoorthy90@gmail.com>
2018-03-21 12:42:57 +0530

    Update CheckServices.java

--- src/android/CheckServices.java
+++ src/android/CheckServices.java
@@ -1,5 +1,7 @@
 package com.example.plugin;
 
+import android.content.pm.PackageManager;
+
 import org.apache.cordova.*;
 
 import org.json.JSONArray;
@@ -13,15 +15,19 @@ public class CheckServices extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
+        JSONObject jo = new JSONObject();
         if (action.equals("check")) {
-			JSONObject jo = new JSONObject();
-            GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
-            int val = googleApiAvailability.isGooglePlayServicesAvailable(this.cordova.getActivity().getApplicationContext());
-            System.out.println("Value is  :"+ val);
-            System.out.println("Value is  :"+ ConnectionResult.SUCCESS);
-			callbackContext.success(jo.put("isGooglePlayServicesAvailable", val == ConnectionResult.SUCCESS));
-            return true;
+
+            try {
+                final String installer = this.cordova.getActivity().getApplicationContext().getPackageManager().getInstallerPackageName(this.cordova.getActivity().getApplicationContext().getPackageName());
+                callbackContext.success(jo.put("type", installer));
+                return true;
+            }
+
+            catch (Exception e) {
+                callbackContext.error(jo.put("error", e.getMessage()));
+                return false;
+            }
 
         } else {
             
